744b28b85383b6129ec473cd3ed3f6df6daf787d
gustavkristiansson <gustav.kristiansson99@gmail.com>
2021-04-24 22:51:23 +0200

    Added project folders and begun working on algoritm

---
+++ A_style_pathfinding_algoritm/Assets/Scripts/Pathfinding.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinding : MonoBehaviour {
+
+    PathFindingGrid grid;
+
+    void Awake() {
+        grid = GetComponent<PathFindingGrid>();
+    }
+
+    void FindPath(Vector3 startPos, Vector3 targetPos) {
+        Node startNode = grid.NodeFromWorldPoint(startPos);
+        Node targetNode = grid.NodeFromWorldPoint(targetPos);
+    }
+}
